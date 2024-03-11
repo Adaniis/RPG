d76d1c16a1a31585c5f43a0a8a2d21d6454056cc
Adaniis <lucas.scheepmans@yahoo.fr>
2024-03-11 16:21:23 +0100

    minor change

--- Assets/Asset Packs/script/mover.cs
+++ Assets/Asset Packs/script/mover.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    
 
-    // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+     void MoveToCursor()
+    {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+         bool hasHit =   Physics.Raycast(ray,out hit);
+            if (hasHit);
+            {
+                GetComponent<NavMeshAgent>().destination = hit.point;
+            }
+    } 
+
+        
+
+ 
     }
 }
